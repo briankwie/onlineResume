b4f18a477b88915f053f0b42277f99b1837b7447
briankwie <46533363+briankwie@users.noreply.github.com>
2019-04-09 20:14:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('awsessentials');
-var modalImg = document.getElementById("img03");
+var modalImg = document.getElementById("img04");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
