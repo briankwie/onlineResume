86aa1e8140c2c21c0245c80d3b780c2f11a06c4e
briankwie <46533363+briankwie@users.noreply.github.com>
2019-05-11 15:38:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ showHideGit.onclick = function() {
     var testGit = document.getElementById('gitDescript');
     var x = document.getElementsByClassName("detailsGit");
     var i;
-    if (testGit.style.display == "block")
+    if (testGit.style.display == "none")
     for (i = 0; i < x.length; i++) {
         x[i].classList.toggle("block");
     }
